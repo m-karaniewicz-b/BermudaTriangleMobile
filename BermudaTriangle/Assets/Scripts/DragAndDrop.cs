1c1d38e89c7b54e1ac8ee4f83d440234d57fc4f0
MKB <cspk00@gmail.com>
2021-03-27 19:45:00 +0100

    Touch screen + new eye

--- BermudaTriangle/Assets/Scripts/DragAndDrop.cs
+++ BermudaTriangle/Assets/Scripts/DragAndDrop.cs
@@ -9,6 +9,7 @@ public class DragAndDrop : MonoBehaviour
 
     private EyeCenter eye;
 
+
     private void Awake()
     {
         eye = FindObjectOfType<EyeCenter>();
@@ -17,56 +18,90 @@ public class DragAndDrop : MonoBehaviour
 
     private void Update()
     {
-        if (heldObject != null)
+        if (Input.touchCount > 0)
         {
-            heldObject.transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Touch touch = Input.GetTouch(0);
 
-            //Rect scrRect = AspectUtility.screenRect;
-
-            //Vector3 minScreenBounds = Camera.main.ScreenToWorldPoint(scrRect.min);
-            //Vector3 maxScreenBounds = Camera.main.ScreenToWorldPoint(scrRect.max);       
-
-            Vector3 minScreenBounds = Camera.main.ScreenToWorldPoint(Vector2.zero);
-            Vector3 maxScreenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
-
-            heldObject.transform.position = new Vector3(
-                Mathf.Clamp(heldObject.transform.position.x, minScreenBounds.x, maxScreenBounds.x),
-                Mathf.Clamp(heldObject.transform.position.y, minScreenBounds.y, maxScreenBounds.y),
-                heldObject.transform.position.z);
-        }
-
-
-        if (Input.GetMouseButtonUp(0))
-        {
             if (heldObject != null)
             {
-                AudioManager.instance.Play("PutDown");
-                VFXManager.SpawnParticleOneshot(VFXManager.instance.dropVFX, Camera.main.ScreenToWorldPoint(Input.mousePosition));
-                heldObject = null;
-            }
-        }
+                UpdateDraggedObjectPosition();
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (!eye.IsInClickDistance())
-            {
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity, dragMask);
-
-                if (hit.collider != null)
+                if (touch.phase == TouchPhase.Ended)
                 {
-                    AudioManager.instance.Play("PickUp");
-                    VFXManager.SpawnParticleOneshot(VFXManager.instance.clickEmptyVFX, Camera.main.ScreenToWorldPoint(Input.mousePosition));
-                    //Debug.Log(hit.collider.gameObject.name);
-                    heldObject = hit.collider.gameObject;
+                    EndTouch();
                 }
-                else
+            }
+            else
+            {
+                if (touch.phase == TouchPhase.Began)
                 {
-                    AudioManager.instance.Play("ClickEmpty");
-                    VFXManager.SpawnParticleOneshot(VFXManager.instance.clickEmptyVFX, Camera.main.ScreenToWorldPoint(Input.mousePosition));
+                    BeginTouch();
                 }
             }
         }
     }
+
+    private void BeginTouch()
+    {
+        if (!eye.IsInClickDistance())
+        {
+            //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity, dragMask);
+
+            RaycastHit2D hit = Physics2D.Raycast(GetTouchPosition(), Camera.main.transform.forward, Mathf.Infinity, dragMask);
+
+            if (hit.collider != null)
+            {
+                AudioManager.instance.Play("PickUp");
+                VFXManager.SpawnParticleOneshot(VFXManager.instance.clickEmptyVFX, GetTouchPosition());
+                //Debug.Log(hit.collider.gameObject.name);
+                heldObject = hit.collider.gameObject;
+            }
+            else
+            {
+                AudioManager.instance.Play("ClickEmpty");
+                VFXManager.SpawnParticleOneshot(VFXManager.instance.clickEmptyVFX, GetTouchPosition());
+            }
+        }
+    }
+
+    private void EndTouch()
+    {
+        AudioManager.instance.Play("PutDown");
+        VFXManager.SpawnParticleOneshot(VFXManager.instance.dropVFX, GetTouchPosition());
+        heldObject = null;
+
+    }
+
+    private void UpdateDraggedObjectPosition()
+    {
+        heldObject.transform.position = GetTouchPosition();
+
+        Vector3 minScreenBounds = Camera.main.ScreenToWorldPoint(Vector2.zero);
+        Vector3 maxScreenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+
+        heldObject.transform.position = new Vector3(
+            Mathf.Clamp(heldObject.transform.position.x, minScreenBounds.x, maxScreenBounds.x),
+            Mathf.Clamp(heldObject.transform.position.y, minScreenBounds.y, maxScreenBounds.y),
+            heldObject.transform.position.z);
+    }
+
+    public static Vector2 GetTouchPosition()
+    {
+        if(Input.touchCount>0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+
+            return touchPos;
+        }
+        else
+        {
+            Debug.LogError("Check for touchCount before calling this!");
+            return Vector2.zero;
+        }
+    }
+
+
 
 }
--- BermudaTriangle/Assets/Scripts/EyeCenter.cs
+++ BermudaTriangle/Assets/Scripts/EyeCenter.cs
@@ -12,48 +12,58 @@ public class EyeCenter : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.touchCount > 0)
         {
-            //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            //RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity, eyeMask);
+            Touch touch = Input.GetTouch(0);
 
-            if (IsInClickDistance())
+            if (touch.phase == TouchPhase.Began)
             {
-                //targetList.RemoveAll(item => item == null);
-
-                if (targetList.Count > 0)
+                if (IsInClickDistance())
                 {
-                    GameManager.instance.AddPoints(1);
-                    AudioManager.instance.Play("Hit");
-                    VFXManager.SpawnParticleOneshot(VFXManager.instance.hitVFX, transform.position);
-
-                    List<MovingTarget> toDestroy = new List<MovingTarget>();
-                    for (int i = 0; i < targetList.Count; i++)
-                    {
-                        toDestroy.Add(targetList[i]);
-
-                        //Debug.Log($"Destroying: {targetList[i].gameObject}");
-                        //Destroy(targetList[i].gameObject);
-                    }
-
-                    for (int i = 0; i < toDestroy.Count; i++)
-                    {
-                        Destroy(toDestroy[i].gameObject);
-                    }
-
-                }
-                else
-                {
-                    AudioManager.instance.Play("Miss");
-                    VFXManager.SpawnParticleOneshot(VFXManager.instance.missVFX, transform.position);
+                    Activate();
                 }
             }
+
+        }
+
+    }
+
+    public void Activate()
+    {
+        //targetList.RemoveAll(item => item == null);
+
+        if (targetList.Count > 0)
+        {
+            GameManager.instance.AddPoints(1);
+            AudioManager.instance.Play("Hit");
+            VFXManager.SpawnParticleOneshot(VFXManager.instance.hitVFX, transform.position);
+
+            List<MovingTarget> toDestroy = new List<MovingTarget>();
+            for (int i = 0; i < targetList.Count; i++)
+            {
+                toDestroy.Add(targetList[i]);
+
+                //Debug.Log($"Destroying: {targetList[i].gameObject}");
+                //Destroy(targetList[i].gameObject);
+            }
+
+            for (int i = 0; i < toDestroy.Count; i++)
+            {
+                Destroy(toDestroy[i].gameObject);
+            }
+
+        }
+        else
+        {
+            AudioManager.instance.Play("Miss");
+            VFXManager.SpawnParticleOneshot(VFXManager.instance.missVFX, transform.position);
+
         }
     }
 
     public bool IsInClickDistance()
     {
-        return Vector2.Distance(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition)) < clickDist;
+        return Vector2.Distance(transform.position, DragAndDrop.GetTouchPosition()) < clickDist;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)

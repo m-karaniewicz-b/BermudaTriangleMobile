8b19d002515d47ffb8216a1d66cb9619d32f8d6b
MKB <cspk00@gmail.com>
2021-04-20 20:18:06 +0200

    Loading

---
+++ BermudaTriangle/Assets/Scripts/Loading.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Loading : MonoBehaviour
+{
+    public TextMeshProUGUI loadingText;
+
+    void Start()
+    {
+        StartCoroutine(LoadSceneAsyncOperation(1));
+    }
+
+    IEnumerator LoadSceneAsyncOperation(int sceneIndex)
+    {
+        AsyncOperation gameLevel = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (gameLevel.progress < 1)
+        {
+            loadingText.text = $"{Mathf.Floor(gameLevel.progress * 100)}%";
+
+            yield return new WaitForEndOfFrame();
+        }
+
+
+    }
+
+}
